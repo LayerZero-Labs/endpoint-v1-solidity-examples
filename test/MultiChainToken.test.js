f4bf727b39677a0f10979cf69317f27091d35b09
Kyle Zarick <kyle@layerzerolabs.org>
2022-01-11 10:08:09 -0500

    adding in working unit test to MultiChainToken

--- test/MultiChainToken.test.js
+++ test/MultiChainToken.test.js
@@ -4,8 +4,8 @@ const { ethers } = require("hardhat");
 describe("MultiChainToken", function () {
 
     beforeEach(async function () {
-        // this.accounts = await ethers.getSigners();
-        // this.owner = this.accounts[0];
+        this.accounts = await ethers.getSigners();
+        this.owner = this.accounts[0];
 
         // use this chainId
         this.chainId = 123;
@@ -16,16 +16,25 @@ describe("MultiChainToken", function () {
 
         // create two MultiChainCounter instances
         const MultiChainToken = await ethers.getContractFactory("MultiChainToken");
-        this.multiChainTokenA = await MultiChainToken.deploy("NAME", "SYM", this.layerZeroEndpointMock.address);
-        this.multiChainTokenB = await MultiChainToken.deploy("NAME", "SYM", this.layerZeroEndpointMock.address);
+        this.multiChainTokenA = await MultiChainToken.deploy("NAME1", "SYM1", this.layerZeroEndpointMock.address);
+        this.multiChainTokenB = await MultiChainToken.deploy("NAME2", "SYM2", this.layerZeroEndpointMock.address);
     });
 
     it("burn local tokens on chain a and mint on chain b", async function () {
-        // let a = await this.multiChainTokenA.balanceOf(this.multiChainTokenA.address);
-        // let b = await this.multiChainTokenB.balanceOf(this.multiChainTokenB.address);
-        await this.multiChainTokenA.approve(this.multiChainTokenA.address, "1000000000");
-        await this.multiChainTokenA.sendTokens(this.chainId, this.multiChainTokenB.address, "1000000000")
-        let b = await this.multiChainTokenB.balanceOf(this.multiChainTokenB.address);
-        // expect(b).to.be.equal("1000000000");
+        // ensure they're both starting from 100000000000000000000
+        let a = await this.multiChainTokenA.balanceOf(this.owner.address);
+        let b = await this.multiChainTokenB.balanceOf(this.owner.address);
+        expect(a).to.be.equal("100000000000000000000");
+        expect(b).to.be.equal("100000000000000000000");
+
+        //approve and send tokens
+        await this.multiChainTokenA.approve(this.multiChainTokenA.address, "69420");
+        await this.multiChainTokenA.sendTokens(this.chainId, this.multiChainTokenB.address, "69420")
+
+        //verify tokens burned on chain a and minted on chain b
+        a = await this.multiChainTokenA.balanceOf(this.owner.address);
+        b = await this.multiChainTokenB.balanceOf(this.owner.address);
+        expect(a).to.be.equal("99999999999999930580");
+        expect(b).to.be.equal("100000000000000069420");
     });
 });
