75177ddf11903ad6740a9ab02e1017026331d4db
lz.wΔrlock() <trileyschwarz@gmail.com>
2022-04-14 20:55:22 -0400

    Cleanup naming conventions in ProxyONFT test

--- test/onft/ProxyONFT.test.js
+++ test/onft/ProxyONFT.test.js
@@ -2,14 +2,14 @@ const { expect } = require("chai")
 const { ethers } = require("hardhat")
 
 describe("ProxyONFT: ", function () {
-    const chainIdSrc = 1
-    const chainIdDst = 2
-    const chainIdDst2 = 3
+    const chainId_A = 1
+    const chainId_B = 2
+    const chainId_C = 3
     const name = "OmnichainNonFungibleToken"
     const symbol = "ONFT"
 
-    let owner, warlock, lzEndpointSrcMock, lzEndpointDstMock, lzEndpointDstMock2
-    let ONFTDst, ONFTDst2, LZEndpointMock, ONFT, ERC721, ERC721Src, ProxyONFT, ProxyONFTSrc
+    let owner, warlock, lzEndpointMockA, lzEndpointMockB, lzEndpointMockC
+    let ONFT_B, ONFT_C, LZEndpointMock, ONFT, ERC721, ERC721Src, ProxyONFT_A, ProxyONFT
 
     before(async function () {
         owner = (await ethers.getSigners())[0]
@@ -22,34 +22,34 @@ describe("ProxyONFT: ", function () {
     })
 
     beforeEach(async function () {
-        lzEndpointSrcMock = await LZEndpointMock.deploy(chainIdSrc)
-        lzEndpointDstMock = await LZEndpointMock.deploy(chainIdDst)
-        lzEndpointDstMock2 = await LZEndpointMock.deploy(chainIdDst2)
+        lzEndpointMockA = await LZEndpointMock.deploy(chainId_A)
+        lzEndpointMockB = await LZEndpointMock.deploy(chainId_B)
+        lzEndpointMockC = await LZEndpointMock.deploy(chainId_C)
 
         // make an ERC721 to mock a previous deploy
         ERC721Src = await ERC721.deploy("ERC721", "ERC721")
         // generate a proxy to allow it to go ONFT
-        ProxyONFTSrc = await ProxyONFT.deploy(lzEndpointSrcMock.address, ERC721Src.address)
+        ProxyONFT_A = await ProxyONFT.deploy(lzEndpointMockA.address, ERC721Src.address)
 
         // create ONFT on dstChains
-        ONFTDst = await ONFT.deploy(name, symbol, lzEndpointDstMock.address)
-        ONFTDst2 = await ONFT.deploy(name, symbol, lzEndpointDstMock2.address)
+        ONFT_B = await ONFT.deploy(name, symbol, lzEndpointMockB.address)
+        ONFT_C = await ONFT.deploy(name, symbol, lzEndpointMockC.address)
 
         // wire the lz endpoints to guide msgs back and forth
-        lzEndpointSrcMock.setDestLzEndpoint(ONFTDst.address, lzEndpointDstMock.address)
-        lzEndpointSrcMock.setDestLzEndpoint(ONFTDst2.address, lzEndpointDstMock2.address)
-        lzEndpointDstMock.setDestLzEndpoint(ProxyONFTSrc.address, lzEndpointSrcMock.address)
-        lzEndpointDstMock.setDestLzEndpoint(ONFTDst2.address, lzEndpointDstMock2.address)
-        lzEndpointDstMock2.setDestLzEndpoint(ProxyONFTSrc.address, lzEndpointSrcMock.address)
-        lzEndpointDstMock2.setDestLzEndpoint(ONFTDst.address, lzEndpointDstMock.address)
+        lzEndpointMockA.setDestLzEndpoint(ONFT_B.address, lzEndpointMockB.address)
+        lzEndpointMockA.setDestLzEndpoint(ONFT_C.address, lzEndpointMockC.address)
+        lzEndpointMockB.setDestLzEndpoint(ProxyONFT_A.address, lzEndpointMockA.address)
+        lzEndpointMockB.setDestLzEndpoint(ONFT_C.address, lzEndpointMockC.address)
+        lzEndpointMockC.setDestLzEndpoint(ProxyONFT_A.address, lzEndpointMockA.address)
+        lzEndpointMockC.setDestLzEndpoint(ONFT_B.address, lzEndpointMockB.address)
 
         // set each contracts source address so it can send to each other
-        await ProxyONFTSrc.setTrustedRemote(chainIdDst, ONFTDst.address)
-        await ProxyONFTSrc.setTrustedRemote(chainIdDst2, ONFTDst2.address)
-        await ONFTDst.setTrustedRemote(chainIdSrc, ProxyONFTSrc.address)
-        await ONFTDst.setTrustedRemote(chainIdDst2, ONFTDst2.address)
-        await ONFTDst2.setTrustedRemote(chainIdSrc, ProxyONFTSrc.address)
-        await ONFTDst2.setTrustedRemote(chainIdDst, ONFTDst.address)
+        await ProxyONFT_A.setTrustedRemote(chainId_B, ONFT_B.address)
+        await ProxyONFT_A.setTrustedRemote(chainId_C, ONFT_C.address)
+        await ONFT_B.setTrustedRemote(chainId_A, ProxyONFT_A.address)
+        await ONFT_B.setTrustedRemote(chainId_C, ONFT_C.address)
+        await ONFT_C.setTrustedRemote(chainId_A, ProxyONFT_A.address)
+        await ONFT_C.setTrustedRemote(chainId_B, ONFT_B.address)
     })
 
     it("swap()", async function () {
@@ -60,37 +60,38 @@ describe("ProxyONFT: ", function () {
         expect(await ERC721Src.ownerOf(tokenId)).to.be.equal(owner.address)
 
         // token doesn't exist on other chain
-        await expect(ONFTDst.ownerOf(tokenId)).to.be.revertedWith("ERC721: operator query for nonexistent token")
+        await expect(ONFT_B.ownerOf(tokenId)).to.be.revertedWith("ERC721: operator query for nonexistent token")
 
         // can transfer token on srcChain as regular erC721
         await ERC721Src.transfer(warlock.address, tokenId)
         expect(await ERC721Src.ownerOf(tokenId)).to.be.equal(warlock.address)
 
         // approve the proxy to swap your token
-        await ERC721Src.connect(warlock).approve(ProxyONFTSrc.address, tokenId)
+        await ERC721Src.connect(warlock).approve(ProxyONFT_A.address, tokenId)
+
         // swaps token to other chain
-        await ProxyONFTSrc.connect(warlock).send(chainIdDst, warlock.address, tokenId, warlock.address, ethers.constants.AddressZero, "0x")
+        await ProxyONFT_A.connect(warlock).send(chainId_B, warlock.address, tokenId, warlock.address, ethers.constants.AddressZero, "0x")
 
         // token is now owned by the proxy contract, because this is the original nft chain
-        expect(await ERC721Src.ownerOf(tokenId)).to.equal(ProxyONFTSrc.address)
+        expect(await ERC721Src.ownerOf(tokenId)).to.equal(ProxyONFT_A.address)
 
         // token received on the dst chain
-        expect(await ONFTDst.ownerOf(tokenId)).to.be.equal(warlock.address)
+        expect(await ONFT_B.ownerOf(tokenId)).to.be.equal(warlock.address)
 
         // can send to other onft contract eg. not the original nft contract chain
-        await ONFTDst.connect(warlock).send(chainIdDst2, warlock.address, tokenId, warlock.address, ethers.constants.AddressZero, "0x")
+        await ONFT_B.connect(warlock).send(chainId_C, warlock.address, tokenId, warlock.address, ethers.constants.AddressZero, "0x")
 
         // token is burned on the sending chain
-        await expect(ONFTDst.ownerOf(tokenId)).to.be.revertedWith("ERC721: operator query for nonexistent token")
+        await expect(ONFT_B.ownerOf(tokenId)).to.be.revertedWith("ERC721: operator query for nonexistent token")
 
         // token received on the dst chain
-        expect(await ONFTDst2.ownerOf(tokenId)).to.be.equal(warlock.address)
+        expect(await ONFT_C.ownerOf(tokenId)).to.be.equal(warlock.address)
 
         // send it back to the original chain
-        await ONFTDst2.connect(warlock).send(chainIdSrc, warlock.address, tokenId, warlock.address, ethers.constants.AddressZero, "0x")
+        await ONFT_C.connect(warlock).send(chainId_A, warlock.address, tokenId, warlock.address, ethers.constants.AddressZero, "0x")
 
         // token is burned on the sending chain
-        await expect(ONFTDst2.ownerOf(tokenId)).to.be.revertedWith("ERC721: operator query for nonexistent token")
+        await expect(ONFT_C.ownerOf(tokenId)).to.be.revertedWith("ERC721: operator query for nonexistent token")
 
         // is received on the original chain
         expect(await ERC721Src.ownerOf(tokenId)).to.be.equal(warlock.address)
