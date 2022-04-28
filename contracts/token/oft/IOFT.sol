0d6f569335d797e618468b707433170eeb7608e5
lz.hᐃnson <hanson@layerzerolabs.org>
2022-04-28 10:45:14 +0800

    OFT Core interface

--- contracts/token/oft/IOFT.sol
+++ contracts/token/oft/IOFT.sol
@@ -2,46 +2,13 @@
 
 pragma solidity ^0.8.0;
 
+import "./IOFTCore.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 /**
  * @dev Interface of the OFT standard
  */
-interface IOFT is IERC20 {
-
-    /**
-     * @dev estimate send token `_tokenId` to (`_dstChainId`, `_toAddress`)
-     * _dstChainId - L0 defined chain id to send tokens too
-     * _toAddress - dynamic bytes array which contains the address to whom you are sending tokens to on the dstChain
-     * _tokenId - token Id to transfer
-     * _amount - amount of the tokens to transfer
-     * _useZro - indicates to use zro to pay L0 fees
-     * _adapterParam - flexible bytes array to indicate messaging adapter services in L0
-     */
-    function estimateSendFee(uint16 _dstChainId, bytes calldata _toAddress, uint _amount, bool _useZro, bytes calldata _adapterParams) external view returns (uint nativeFee, uint zroFee);
-
-
-    /**
-     * @dev send `_amount` amount of token to (`_dstChainId`, `_toAddress`)
-     * `_toAddress` can be any size depending on the `dstChainId`.
-     * `_amount` the quantity of tokens in wei
-     * `_refundAddress` the address LayerZero refunds if too much message fee is sent
-     * `_zroPaymentAddress` set to address(0x0) if not paying in ZRO (LayerZero Token)
-     * `_adapterParams` is a flexible bytes array to indicate messaging adapter services
-     */
-    function send(uint16 _dstChainId, bytes calldata _toAddress, uint _amount, address payable _refundAddress, address _zroPaymentAddress, bytes calldata _adapterParams) external payable;
-
-    /**
-     * @dev send `_amount` amount of token to (`_dstChainId`, `_toAddress`) from `_from`
-     * `_dstChainId` the destination chain identifier
-     * `_toAddress` can be any size depending on the `dstChainId`.
-     * `_amount` the quantity of tokens in wei
-     * `_refundAddress` the address LayerZero refunds if too much message fee is sent
-     * `_zroPaymentAddress` set to address(0x0) if not paying in ZRO (LayerZero Token)
-     * `_adapterParams` is a flexible bytes array to indicate messaging adapter services
-     */
-    function sendFrom(address _from, uint16 _dstChainId, bytes calldata _toAddress, uint _amount, address payable _refundAddress, address _zroPaymentAddress, bytes calldata _adapterParams) external payable;
-
+interface IOFT is IOFTCore, IERC20 {
     /**
      * @dev returns the type of OFT
      */
@@ -51,16 +18,4 @@ interface IOFT is IERC20 {
      * @dev returns the total amount of tokens across all chains
      */
     function getGlobalSupply() external returns (uint);
-
-    /**
-     * @dev Emitted when `_amount` tokens are moved from the `_sender` to (`_dstChainId`, `_toAddress`)
-     * `_nonce` is the outbound nonce
-     */
-    event SendToChain(address indexed _sender, uint16 indexed _dstChainId, bytes indexed _toAddress, uint _amount, uint64 _nonce);
-
-    /**
-     * @dev Emitted when `_amount` tokens are received from `_srcChainId` into the `_toAddress` on the local chain.
-     `_nonce` is the inbound nonce.
-     */
-    event ReceiveFromChain(uint16 indexed _srcChainId, bytes indexed _srcAddress, address indexed _toAddress, uint _amount, uint64 _nonce);
 }
--- contracts/token/oft/extension/ProxyOFT.sol
+++ contracts/token/oft/extension/ProxyOFT.sol
@@ -3,38 +3,56 @@
 pragma solidity ^0.8.0;
 
 import "../../../lzApp/NonblockingLzApp.sol";
+import "../IOFTCore.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-contract ProxyOFT is NonblockingLzApp {
+contract ProxyOFT is NonblockingLzApp, IOFTCore {
     using SafeERC20 for IERC20;
 
     IERC20 public immutable token;
-
-    event SendToChain(address indexed _sender, uint16 indexed _dstChainId, bytes indexed _toAddress, uint _amount, uint64 _nonce);
-    event ReceiveFromChain(uint16 _srcChainId, address _toAddress, uint _amount, uint64 _nonce);
 
     constructor(address _lzEndpoint, address _proxyToken) NonblockingLzApp(_lzEndpoint) {
         token = IERC20(_proxyToken);
     }
 
-    function send(uint16 _dstChainId, bytes calldata _toAddress, uint _amount, address payable _refundAddress, address _zroPaymentAddress, bytes calldata _adapterParam) public payable virtual {
-        _send(_msgSender(), _dstChainId, _toAddress, _amount, _refundAddress, _zroPaymentAddress, _adapterParam);
+    function send(
+        uint16 _dstChainId,
+        bytes calldata _toAddress,
+        uint _amount,
+        address payable _refundAddress,
+        address _zroPaymentAddress,
+        bytes calldata _adapterParams
+    ) public payable virtual override {
+        _send(_msgSender(), _dstChainId, _toAddress, _amount, _refundAddress, _zroPaymentAddress, _adapterParams);
     }
 
-    function estimateSendFee(uint16 _dstChainId, bytes calldata _toAddress, uint _amount, bool _useZro, bytes calldata _adapterParams) external view virtual returns (uint nativeFee, uint zroFee) {
+    function sendFrom(
+        address, /* _from */
+        uint16, /* _dstChainId */
+        bytes calldata, /* _toAddress */
+        uint, /* _amount */
+        address payable, /* _refundAddress */
+        address, /* _zroPaymentAddress */
+        bytes calldata /* _adapterParams */
+    ) public payable virtual override {
+        revert("LzSend: no implementer for ProxyOFT");
+    }
+
+    function estimateSendFee(
+        uint16 _dstChainId,
+        bytes calldata _toAddress,
+        uint _amount,
+        bool _useZro,
+        bytes calldata _adapterParams
+    ) external view virtual override returns (uint nativeFee, uint zroFee) {
         // mock the payload for send()
         bytes memory payload = abi.encode(_toAddress, _amount);
         return lzEndpoint.estimateFees(_dstChainId, address(this), payload, _useZro, _adapterParams);
     }
 
-    // using the proxy Token's total supply as source of truth
-    function totalSupply() public view virtual returns (uint) {
-        return token.totalSupply();
-    }
-
     function _nonblockingLzReceive(
         uint16 _srcChainId,
-        bytes memory, /*_srcAddress*/
+        bytes memory _srcAddress,
         uint64 _nonce,
         bytes memory _payload
     ) internal virtual override {
@@ -47,10 +65,18 @@ contract ProxyOFT is NonblockingLzApp {
 
         _creditTo(_srcChainId, toAddress, amount);
 
-        emit ReceiveFromChain(_srcChainId, toAddress, amount, _nonce);
+        emit ReceiveFromChain(_srcChainId, _srcAddress, toAddress, amount, _nonce);
     }
 
-    function _send(address _from, uint16 _dstChainId, bytes memory _toAddress, uint _amount, address payable _refundAddress, address _zroPaymentAddress, bytes calldata _adapterParam) internal virtual {
+    function _send(
+        address _from,
+        uint16 _dstChainId,
+        bytes memory _toAddress,
+        uint _amount,
+        address payable _refundAddress,
+        address _zroPaymentAddress,
+        bytes calldata _adapterParam
+    ) internal virtual {
         _debitFrom(_from, _dstChainId, _toAddress, _amount);
 
         bytes memory payload = abi.encode(_toAddress, _amount);
