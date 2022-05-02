58ab7ebc1ed3e69cb7aa9afa5e6ad5b5eae12f1e
lz.hᐃnson <hanson@layerzerolabs.org>
2022-05-02 20:35:40 +0800

    transfer token from `_from`

--- contracts/token/oft/extension/BasedOFT.sol
+++ contracts/token/oft/extension/BasedOFT.sol
@@ -6,8 +6,8 @@ import "../OFT.sol";
 contract BasedOFT is OFT {
     constructor(string memory _name, string memory _symbol, address _lzEndpoint, uint _globalSupply) OFT(_name, _symbol, _lzEndpoint, _globalSupply) {}
 
-    function _debitFrom(address, uint16, bytes memory, uint _amount) internal virtual override {
-        _transfer(_msgSender(), address(this), _amount);
+    function _debitFrom(address _from, uint16, bytes memory, uint _amount) internal virtual override {
+        _transfer(_from, address(this), _amount);
     }
 
     function _creditTo(uint16, address _toAddress, uint _amount) internal virtual override {
