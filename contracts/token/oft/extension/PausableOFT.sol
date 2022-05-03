c2fd91aab64c6706424de2fe65351f62f1a91f1a
lz.hᐃnson <hanson@layerzerolabs.org>
2022-05-03 14:20:36 +0800

    update PausableOFT

--- contracts/token/oft/extension/PausableOFT.sol
+++ contracts/token/oft/extension/PausableOFT.sol
@@ -9,8 +9,8 @@ import "@openzeppelin/contracts/security/Pausable.sol";
 contract PausableOFT is OFT, Pausable {
     constructor(string memory _name, string memory _symbol, address _lzEndpoint) OFT(_name, _symbol, _lzEndpoint) {}
 
-    function _debitFrom(address _from, uint16, bytes memory, uint _amount) internal virtual override whenNotPaused {
-        _burn(_from, _amount);
+    function _debitFrom(address _from, uint16 _dstChainId, bytes memory _toAddress, uint _amount) internal virtual override whenNotPaused {
+        super._debitFrom(_from, _dstChainId, _toAddress, _amount);
     }
 
     function pauseSendTokens(bool pause) external onlyOwner {
