749c7ca61bc9fb69678cb4e09ca643854876a68d
exakoss <danielkossman@yahoo.com>
2022-05-14 20:40:17 -0400

    feat: AdvancedONFT721 contract and test

---
+++ contracts/token/onft/extension/AdvancedONFT721.sol
@@ -0,0 +1,154 @@
+// SPDX-License-Identifier: BUSL-1.1
+
+pragma solidity ^0.8;
+
+import "../ONFT721.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+/// @title Interface of the AdvancedONFT standard
+/// @author exakoss
+/// @notice this implementation supports: batch mint, payable public and private mint, reveal of metadata and EIP-2981 on-chain royalties
+contract AdvancedONFT721 is ONFT721, ReentrancyGuard {
+    using Strings for uint;
+    
+    uint public price = 0;
+    uint public nextMintId;
+    uint public maxMintId;
+    uint public maxTokensPerMint;
+
+    // royalty fee in basis points (i.e. 100% = 10000, 1% = 100)
+    uint royaltyBasisPoints = 500;
+    // address for withdrawing money and receiving royalties, separate from owner
+    address payable beneficiary;
+
+    string public contractURI;
+    string private baseURI;
+    string private hiddenMetadataURI;
+
+    mapping(address => uint256) public _allowList;
+
+    bool public _publicSaleStarted;
+    bool public _saleStarted;
+    bool revealed;
+
+    /// @notice Constructor for the AdvancedONFT
+    /// @param _name the name of the token
+    /// @param _symbol the token symbol
+    /// @param _layerZeroEndpoint handles message transmission across chains
+    /// @param _startMintId the starting mint number on this chain, excluded
+    /// @param _endMintId the max number of mints on this chain
+    /// @param _maxTokensPerMint the max number of tokens that could be minted in a single transaction
+    /// @param _baseTokenURI the base URI for computing the tokenURI
+    /// @param _hiddenURI the URI for computing the hiddenMetadataUri
+    constructor(string memory _name, string memory _symbol, address _layerZeroEndpoint, uint _startMintId, uint _endMintId, uint _maxTokensPerMint, string memory _baseTokenURI, string memory _hiddenURI) ONFT721(_name, _symbol, _layerZeroEndpoint) {
+        nextMintId = _startMintId;
+        maxMintId = _endMintId;
+        maxTokensPerMint = _maxTokensPerMint;
+        //set default beneficiary to owner
+        beneficiary = payable(msg.sender);
+        baseURI = _baseTokenURI;
+        hiddenMetadataURI = _hiddenURI;
+    }
+
+    /// @notice Mint your ONFTs
+    function publicMint(uint256 _nbTokens) external payable {
+        require(_publicSaleStarted == true, "AdvancedONFT721: Public sale has not started yet!");
+        require(_saleStarted == true, "AdvancedONFT721: Sale has not started yet!");
+        require(_nbTokens != 0, "AdvancedONFT721: Cannot mint 0 tokens!");
+        require(_nbTokens <= maxTokensPerMint, "AdvancedONFT721: You cannot mint more than maxTokensPerMint tokens at once!");
+        require(nextMintId + _nbTokens <= maxMintId, "AdvancedONFT721: max mint limit reached");
+        require(_nbTokens * price <= msg.value, "AdvancedONFT721: Inconsistent amount sent!");
+
+        //using a local variable, _mint and ++X pattern to save gas
+        uint256 local_nextMintId = nextMintId;
+        for (uint256 i; i < _nbTokens; i++) {
+            _mint(msg.sender, ++local_nextMintId);
+        }
+        nextMintId = local_nextMintId;
+    }
+
+    /// @notice Mint your ONFTs, whitelisted addresses only
+    function mint(uint256 _nbTokens) external payable {
+        require(_saleStarted == true, "AdvancedONFT721: Sale has not started yet!");
+        require(_nbTokens != 0, "AdvancedONFT721: Cannot mint 0 tokens!");
+        require(_nbTokens <= maxTokensPerMint, "AdvancedONFT721: You cannot mint more than maxTokensPerMint tokens at once!");
+        require(nextMintId + _nbTokens <= maxMintId, "AdvancedONFT721: max mint limit reached");
+        require(_nbTokens * price <= msg.value, "AdvancedONFT721: Inconsistent amount sent!");
+        require(_allowList[msg.sender] >= _nbTokens, "AdvancedONFT721: You exceeded your token limit.");
+
+        _allowList[msg.sender] -= _nbTokens;
+
+        //using a local variable, _mint and ++X pattern to save gas
+        uint256 local_nextMintId = nextMintId;
+        for (uint256 i; i < _nbTokens; i++) {
+            _mint(msg.sender, ++local_nextMintId);
+        }
+        nextMintId = local_nextMintId;
+    }
+
+    function setPrice(uint newPrice) external onlyOwner {
+        price = newPrice;
+    }
+
+    function withdraw() public virtual onlyOwner {
+        require(beneficiary != address(0), "AdvancedONFT721: Beneficiary not set!");
+        uint256 _balance = address(this).balance;
+        require(payable(beneficiary).send(_balance));
+    }
+
+    function royaltyInfo(uint256, uint256 salePrice) external view returns (address receiver, uint256 royaltyAmount) {
+        receiver = beneficiary;
+        royaltyAmount = salePrice * royaltyBasisPoints / 10000;
+    }
+
+    function setContractURI(string memory _contractURI) public onlyOwner {
+        contractURI = _contractURI;
+    }
+
+    function setBaseURI(string memory uri) public onlyOwner {
+        baseURI = uri;
+    }
+
+    function setRoyaltyFee(uint256 _royaltyBasisPoints) external onlyOwner {
+        royaltyBasisPoints = _royaltyBasisPoints;
+    }
+
+    function setBeneficiary(address payable _beneficiary) external onlyOwner {
+        beneficiary = _beneficiary;
+    }
+
+    function setHiddenMetadataUri(string memory _hiddenMetadataUri) external onlyOwner {
+        hiddenMetadataURI = _hiddenMetadataUri;
+    }
+
+    function setAllowList(address[] calldata addresses) external onlyOwner {
+        for (uint256 i = 0; i < addresses.length; i++) {
+            _allowList[addresses[i]] = maxTokensPerMint;
+        }
+    }
+
+    function flipRevealed() external onlyOwner {
+        revealed = !revealed;
+    }
+
+    function flipSaleStarted() external onlyOwner {
+        _saleStarted = !_saleStarted;
+    }
+
+    function flipPublicSaleStarted() external onlyOwner {
+        _publicSaleStarted = !_publicSaleStarted;
+    }
+
+    // The following functions are overrides required by Solidity.
+    function _baseURI() internal view override returns (string memory) {
+        return baseURI;
+    }
+
+    function tokenURI(uint256 tokenId) public view override(ERC721) returns (string memory) {
+        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+        if (!revealed) {
+            return hiddenMetadataURI;
+        }
+        return string(abi.encodePacked(_baseURI(), tokenId.toString()));
+    }
+}
---
+++ test/onft/AdvancedONFT721.test.js
@@ -0,0 +1,71 @@
+const { expect } = require("chai")
+const { ethers } = require("hardhat")
+
+describe("AdvancedONFT721: ", function () {
+    const chainIdSrc = 1
+    const chainIdDst = 2
+    const name = "AdvancedONFT"
+    const symbol = "AONFT"
+
+    let owner, lzEndpointSrcMock, lzEndpointDstMock, ONFTSrc, ONFTDst, LZEndpointMock, ONFT, ONFTSrcIds, ONFTDstIds
+    // AONFT specific variables
+    let maxTokensPerMint, baseURI, hiddenURI
+
+
+    before(async function () {
+        owner = (await ethers.getSigners())[0]
+
+        LZEndpointMock = await ethers.getContractFactory("LZEndpointMock")
+        ONFT = await ethers.getContractFactory("AdvancedONFT721")
+        ONFTSrcIds = [0, 2] // [startID, endID]... only allowed to mint two ONFTs
+        ONFTDstIds = [2, 4] // [startID, endID]... only allowed to mint two ONFTs
+        maxTokensPerMint = 2
+        baseURI = ''
+        hiddenURI = ''
+
+    })
+
+    beforeEach(async function () {
+        lzEndpointSrcMock = await LZEndpointMock.deploy(chainIdSrc)
+        lzEndpointDstMock = await LZEndpointMock.deploy(chainIdDst)
+
+        // create two UniversalONFT instances
+        ONFTSrc = await ONFT.deploy(name, symbol, lzEndpointSrcMock.address, ONFTSrcIds[0], ONFTSrcIds[1], maxTokensPerMint, baseURI, hiddenURI)
+        ONFTDst = await ONFT.deploy(name, symbol, lzEndpointDstMock.address, ONFTDstIds[0], ONFTDstIds[1], maxTokensPerMint, baseURI, hiddenURI)
+
+        lzEndpointSrcMock.setDestLzEndpoint(ONFTDst.address, lzEndpointDstMock.address)
+        lzEndpointDstMock.setDestLzEndpoint(ONFTSrc.address, lzEndpointSrcMock.address)
+
+        // set each contracts source address so it can send to each other
+        await ONFTSrc.setTrustedRemote(chainIdDst, ONFTDst.address) // for A, set B
+        await ONFTDst.setTrustedRemote(chainIdSrc, ONFTSrc.address) // for B, set A
+    })
+
+    it("sendFrom() - mint on the source chain and send ONFT to the destination chain", async function () {
+        //activate public sale
+        await ONFTSrc.flipSaleStarted()
+        await ONFTSrc.flipPublicSaleStarted()
+        // mint ONFTs
+        const newId = await ONFTSrc.nextMintId() + 1
+        await ONFTSrc.publicMint(2)
+
+        // verify the owner of the token is on the source chain
+        expect(await ONFTSrc.ownerOf(newId)).to.be.equal(owner.address)
+
+        // approve and send ONFT
+        await ONFTSrc.approve(ONFTSrc.address, newId)
+        // v1 adapterParams, encoded for version 1 style, and 250k gas quote
+        const adapterParam = ethers.utils.solidityPack(["uint16", "uint256"], [1, 250000])
+
+        await ONFTSrc.sendFrom(owner.address, chainIdDst, owner.address, newId, owner.address, "0x000000000000000000000000000000000000dEaD", adapterParam)
+
+        // verify the owner of the token is no longer on the source chain
+        await expect(ONFTSrc.ownerOf(newId)).to.revertedWith("ERC721: owner query for nonexistent token")
+
+        // verify the owner of the token is on the destination chain
+        expect(await ONFTDst.ownerOf(newId)).to.not.equal(owner)
+
+        // hit the max mint on the source chain
+        await expect(ONFTSrc.publicMint(1)).to.revertedWith("AdvancedONFT721: max mint limit reached")
+    })
+})
