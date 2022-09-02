0aad02d400add3f7a32ba151ab97afcb6dfbca0d
lz.sirΔrthurmoney() <kyle@layerzerolabs.org>
2022-09-01 20:34:39 -0700

    revert naming

--- contracts/examples/ExampleOFT20.sol
+++ contracts/examples/ExampleOFT20.sol
@@ -7,5 +7,5 @@ import "../token/oft/OFT.sol";
 /// @title A LayerZero OmnichainFungibleToken example using OFT
 /// @notice Works in tandem with a BasedOFT. Use this to contract on for all NON-BASE chains. It burns tokens on send(), and mints on receive tokens form other chains.
 contract ExampleOFT is OFT {
-    constructor(address _layerZeroEndpoint) OFT("NativeOFT", "NPOFT", _layerZeroEndpoint) {}
+    constructor(address _layerZeroEndpoint) OFT("OFT", "OFT", _layerZeroEndpoint) {}
 }
