785119dfe653c050e5edfee2ef1b9cb08cd94f86
tushargargght <tushargargght@gmail.com>
2022-11-29 16:48:47 +0530

    fix: cross-chain test

--- test/OmniCounter.test.js
+++ test/OmniCounter.test.js
@@ -3,28 +3,30 @@ const { ethers } = require("hardhat")
 
 describe("OmniCounter", function () {
     beforeEach(async function () {
-        // use this chainId
-        this.chainId = 123
+        // use this chainIds
+        this.chainIdA = 101
+        this.chainIdB = 102
 
-        // create a LayerZero Endpoint mock for testing
+        // create LayerZero Endpoints mock for testing
         const LayerZeroEndpointMock = await ethers.getContractFactory("LZEndpointMock")
-        this.lzEndpointMock = await LayerZeroEndpointMock.deploy(this.chainId)
+        this.lzEndpointMockA = await LayerZeroEndpointMock.deploy(this.chainIdA)
+        this.lzEndpointMockB = await LayerZeroEndpointMock.deploy(this.chainIdB)
 
-        // create two OmniCounter instances
+        // create two OmniCounter instances on separate chains
         const OmniCounter = await ethers.getContractFactory("OmniCounter")
-        this.omniCounterA = await OmniCounter.deploy(this.lzEndpointMock.address)
-        this.omniCounterB = await OmniCounter.deploy(this.lzEndpointMock.address)
+        this.omniCounterA = await OmniCounter.deploy(this.lzEndpointMockA.address)
+        this.omniCounterB = await OmniCounter.deploy(this.lzEndpointMockB.address)
 
-        this.lzEndpointMock.setDestLzEndpoint(this.omniCounterA.address, this.lzEndpointMock.address)
-        this.lzEndpointMock.setDestLzEndpoint(this.omniCounterB.address, this.lzEndpointMock.address)
+        this.lzEndpointMockA.setDestLzEndpoint(this.omniCounterB.address, this.lzEndpointMockB.address)
+        this.lzEndpointMockB.setDestLzEndpoint(this.omniCounterA.address, this.lzEndpointMockA.address)
 
         // set each contracts source address so it can send to each other
         this.omniCounterA.setTrustedRemote(
-            this.chainId,
+            this.chainIdB,
             ethers.utils.solidityPack(["address", "address"], [this.omniCounterB.address, this.omniCounterA.address])
         )
         this.omniCounterB.setTrustedRemote(
-            this.chainId,
+            this.chainIdA,
             ethers.utils.solidityPack(["address", "address"], [this.omniCounterA.address, this.omniCounterB.address])
         )
     })
@@ -36,12 +38,12 @@ describe("OmniCounter", function () {
 
         // instruct each OmniCounter to increment the other OmniCounter
         // counter A increments counter B
-        await this.omniCounterA.incrementCounter(this.chainId, { value: ethers.utils.parseEther("0.5") })
+        await this.omniCounterA.incrementCounter(this.chainIdB, { value: ethers.utils.parseEther("0.5") })
         expect(await this.omniCounterA.counter()).to.be.equal(0) // still 0
         expect(await this.omniCounterB.counter()).to.be.equal(1) // now its 1
 
         // counter B increments counter A
-        await this.omniCounterB.incrementCounter(this.chainId, { value: ethers.utils.parseEther("0.5") })
+        await this.omniCounterB.incrementCounter(this.chainIdA, { value: ethers.utils.parseEther("0.5") })
         expect(await this.omniCounterA.counter()).to.be.equal(1) // now its 1
         expect(await this.omniCounterB.counter()).to.be.equal(1) // still 1
     })
