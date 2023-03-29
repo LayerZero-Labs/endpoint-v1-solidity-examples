031739c63b72238442e567f460e3aba9f0c4cae0
lz.sirΔrthurmoney() <kyle@layerzerolabs.org>
2023-03-29 15:09:41 -0700

    Replacing BaseOFT example with OFTV2 example in README.md

---
+++ deploy/ExampleOFTV2.js
@@ -0,0 +1,22 @@
+const LZ_ENDPOINTS = require("../constants/layerzeroEndpoints.json")
+const {ethers} = require("hardhat");
+
+module.exports = async function ({ deployments, getNamedAccounts }) {
+    const { deploy } = deployments
+    const { deployer } = await getNamedAccounts()
+    console.log(`>>> your address: ${deployer}`)
+
+    const lzEndpointAddress = LZ_ENDPOINTS[hre.network.name]
+    console.log(`[${hre.network.name}] Endpoint Address: ${lzEndpointAddress}`)
+    const globalSupply = ethers.utils.parseUnits("1000000", 18)
+    const sharedDecimals = 6;
+
+    await deploy("ExampleOFTV2", {
+        from: deployer,
+        args: [lzEndpointAddress, globalSupply, sharedDecimals],
+        log: true,
+        waitConfirmations: 1,
+    })
+}
+
+module.exports.tags = ["ExampleOFTV2"]
---
+++ tasks/oftv2Send.js
@@ -0,0 +1,51 @@
+const CHAIN_ID = require("../constants/chainIds.json")
+
+module.exports = async function (taskArgs, hre) {
+    let signers = await ethers.getSigners()
+    let owner = signers[0]
+    let toAddress = owner.address;
+    let qty = ethers.utils.parseEther(taskArgs.qty)
+
+    let localContract, remoteContract;
+
+    if(taskArgs.contract) {
+        localContract = taskArgs.contract;
+        remoteContract = taskArgs.contract;
+    } else {
+        localContract = taskArgs.localContract;
+        remoteContract = taskArgs.remoteContract;
+    }
+
+    if(!localContract || !remoteContract) {
+        console.log("Must pass in contract name OR pass in both localContract name and remoteContract name")
+        return
+    }
+
+    let toAddressBytes = ethers.utils.defaultAbiCoder.encode(['address'],[toAddress])
+
+    // get remote chain id
+    const remoteChainId = CHAIN_ID[taskArgs.targetNetwork]
+
+    // get local contract
+    const localContractInstance = await ethers.getContract(localContract)
+
+    // quote fee with default adapterParams
+    let adapterParams = ethers.utils.solidityPack(["uint16", "uint256"], [1, 200000]) // default adapterParams example
+
+    let fees = await localContractInstance.estimateSendFee(remoteChainId, toAddressBytes, qty, false, adapterParams)
+    console.log(`fees[0] (wei): ${fees[0]} / (eth): ${ethers.utils.formatEther(fees[0])}`)
+
+    let tx = await (
+        await localContractInstance.sendFrom(
+            owner.address,                 // 'from' address to send tokens
+            remoteChainId,                 // remote LayerZero chainId
+            toAddressBytes,                     // 'to' address to send tokens
+            qty,                           // amount of tokens to send (in wei)
+            [owner.address, ethers.constants.AddressZero, "0x"],
+            { value: fees[0] }
+        )
+    ).wait()
+    console.log(`✅ Message Sent [${hre.network.name}] sendTokens() to OFT @ LZ chainId[${remoteChainId}] token:[${toAddress}]`)
+    console.log(` tx: ${tx.transactionHash}`)
+    console.log(`* check your address [${owner.address}] on the destination chain, in the ERC20 transaction tab !"`)
+}
