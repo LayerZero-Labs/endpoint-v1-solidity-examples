104a035ddb44d39cd46bee50120f50173c7edf64
0xIryna <iryna@layerzerolabs.org>
2023-05-31 17:59:26 -0700

    renamed _totalSupply -> _outboundAmount in ProxyOFTV2View

--- contracts/token/oft/v2/precrime/ProxyOFTV2View.sol
+++ contracts/token/oft/v2/precrime/ProxyOFTV2View.sol
@@ -8,12 +8,12 @@ import "../ProxyOFTV2.sol";
 contract ProxyOFTV2View is OFTV2View {
     constructor(address _oft) OFTV2View(_oft) {}
 
-    function lzReceive(uint16 _srcChainId, bytes32 _scrAddress, bytes memory _payload, uint _totalSupply) external view virtual override returns (uint) {
+    function lzReceive(uint16 _srcChainId, bytes32 _scrAddress, bytes memory _payload, uint _outboundAmount) external view virtual override returns (uint) {
         require(_isPacketFromTrustedRemote(_srcChainId, _scrAddress), "ProxyOFTV2View: not trusted remote");
         uint amount = _decodePayload(_payload);
 
-        require(_totalSupply >= amount, "ProxyOFTV2View: transfer amount exceeds locked amount");
-        return _totalSupply - amount;
+        require(_outboundAmount >= amount, "ProxyOFTV2View: transfer amount exceeds locked amount");
+        return _outboundAmount - amount;
     }
 
     function isProxy() external pure override returns (bool) {
