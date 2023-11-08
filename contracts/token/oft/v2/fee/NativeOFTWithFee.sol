3a5bd3d5f4af7c910798efb4c158c45d1fc406b4
lz.sirΔrthurmoney() <kyle@layerzerolabs.org>
2023-11-07 18:14:15 -0800

    updating require

--- contracts/token/oft/v2/fee/NativeOFTWithFee.sol
+++ contracts/token/oft/v2/fee/NativeOFTWithFee.sol
@@ -31,12 +31,12 @@ contract NativeOFTWithFee is OFTWithFee, ReentrancyGuard {
     ************************************************************************/
     function sendFrom(address _from, uint16 _dstChainId, bytes32 _toAddress, uint _amount, uint _minAmount, LzCallParams calldata _callParams) public payable virtual override {
         _amount = _send(_from, _dstChainId, _toAddress, _amount, _callParams.refundAddress, _callParams.zroPaymentAddress, _callParams.adapterParams);
-        require(_amount >= _minAmount, "BaseOFTWithFee: amount is less than minAmount");
+        require(_amount >= _minAmount, "NativeOFTWithFee: amount is less than minAmount");
     }
 
     function sendAndCall(address _from, uint16 _dstChainId, bytes32 _toAddress, uint _amount, uint _minAmount, bytes calldata _payload, uint64 _dstGasForCall, LzCallParams calldata _callParams) public payable virtual override {
         _amount = _sendAndCall(_from, _dstChainId, _toAddress, _amount, _payload, _dstGasForCall, _callParams.refundAddress, _callParams.zroPaymentAddress, _callParams.adapterParams);
-        require(_amount >= _minAmount, "BaseOFTWithFee: amount is less than minAmount");
+        require(_amount >= _minAmount, "NativeOFTWithFee: amount is less than minAmount");
     }
 
     function _send(address _from, uint16 _dstChainId, bytes32 _toAddress, uint _amount, address payable _refundAddress, address _zroPaymentAddress, bytes memory _adapterParams) internal virtual override returns (uint amount) {
